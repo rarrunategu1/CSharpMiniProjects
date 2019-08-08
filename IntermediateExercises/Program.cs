b8fcb67d299c2bedde558a089caee378eb2df620
rarrunategu1 <parker.rose@ymail.com>
2019-08-07 20:44:49 -0400

    Added start method for stopwatch which stores the time that the stopwatch started

--- IntermediateExercises/Program.cs
+++ IntermediateExercises/Program.cs
@@ -2,11 +2,31 @@
 
 namespace IntermediateExercises
 {
+    public class Stopwatch
+    {
+
+        //provide method to start the stopwatch
+        public void StartTime()
+        {
+            DateTime start = DateTime.Now;
+        }
+
+        //provide method to stop the stopwatch
+
+        //duration between start and stop in TimeSpand
+
+        //diplay duration on the console
+
+        //should be able to use stopwatch multiple times
+
+        //class should throw and Invalid Operation Exception if it's started twice in a row    }
+    }
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Stopwatch stopWatch = new Stopwatch();
+            stopWatch.StartTime();
         }
     }
 }
